743638d8b042f34df4520abdf0d3ff26e19f579e
Sarah Leong <sleong20@stuy.edu>
2021-09-29 20:37:21 -0400

    initialized and printed pointers of each variable

--- pointers.c
+++ pointers.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 
+// initialized variables
 char c;
 int i;
 long l;
 
-// char *cp = &c;
-// int *ip = &p;
-// long *lp = &l;
+// initialized pointers
+char *cp = &c;
+int *ip = &i;
+long *lp = &l;
 
 
 int main() {
-    printf("address of c hex: %p \t address of c dec: %lu\n", &c, &c);
-    printf("address of i hex: %p \t address of i dec: %lu\n", &i, &i);
-    printf("address of l hex: %p \t address of l dec: %lu\n", &l, &l);
+    printf("address of c hex: %p \t dec: %lu\n", &c, &c);
+    printf("address of i hex: %p \t dec: %lu\n", &i, &i);
+    printf("address of l hex: %p \t dec: %lu\n\n", &l, &l);
+    // the addresses are each 4 bits away from each other
+    
+    printf("value of c's pointer hex: %p \t dec: %lu\n", cp,cp);
+    printf("value of i's pointer hex: %p \t dec: %lu\n", ip,ip);
+    printf("value of l's pointer hex: %p \t dec: %lu\n", lp,lp);
+
     return 0;
 }
